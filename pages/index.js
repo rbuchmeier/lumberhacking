38a8cce68d01d686ded0176d6127dd6a9520b7e6
mariomarine <rwbleo@gmail.com>
2020-07-22 09:19:52 -0600

    Add new favicon

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="container">
       <Head>
         <title>LumberHacking LLC</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
 
       <main>
